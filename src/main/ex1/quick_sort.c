a31599b6fc1a1a6da2def7b073571c9f25de8645
Matteo Saracino <matteo.saracino@edu.unito.it>
2024-11-01 18:47:38 +0100

    Added some quick sort's function - ex1 - quick sort

---
+++ src/main/ex1/quick_sort.c
@@ -0,0 +1,31 @@
+#include "quick_sort.h"
+
+void swap(void *a, void *b) {
+
+}
+
+int partition(void *base, size_t size, size_t left, size_t right, int (*compar)(const void*, const void*)) {
+    void *pivot = base + right * size;
+    size_t i = left;
+
+    for (size_t j = left; j < right; j++) {
+        if (compar(base + j * size, pivot) < 0) {
+            swap(base + i * size, base + j * size);
+            i++;
+        }
+    }
+
+    swap(base + i * size, base + right * size);
+
+    return i;
+}
+
+void quick_sort_rec(void *base, size_t nitems, size_t size, size_t left, size_t right, int (*compar)(const void*, const void*)) {
+    
+}
+
+void quick_sort(void *base, size_t nitems, size_t size, int (*compar)(const void*, const void*)) {
+    if (nitems > 1) {
+        quick_sort_rec(base, nitems, size, 0, nitems - 1, compar);
+    }
+}
--- src/main/ex1/quick_sort.h
+++ src/main/ex1/quick_sort.h
@@ -3,6 +3,17 @@
 #include <string.h>
 
 /**
+ * @brief Partition the array using the last element as a pivot
+ * @param base Is a pointer to the first element of the array to sort
+ * @param left Index of the most left element of the array
+ * @param right Index of the most right element of the array
+ * @param size Is the size (in bytes) of each element of the array
+ * @param compar It is the criterion according to which to sort the data (given two pointers to elements of the array, it returns a number greater than, equal to or less than zero if the first argument is respectively greater than, equal to or less than the second)
+ * @return Index of the pivot
+ */
+int partition(void *base, size_t size, size_t left, size_t right, int (*compar)(const void*, const void*));
+
+/**
  * @brief Quick sort of generic array
  * @param base Is a pointer to the first element of the array to sort
  * @param nitems Is the number of elements in the array to sort
