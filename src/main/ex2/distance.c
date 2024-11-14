ce22e3e895e3525d6b2b364b8b7323b218d1bd98
Matteo Saracino <matteo.saracino@edu.unito.it>
2024-11-14 17:38:50 +0100

    Added edit distance function - ex2

--- src/main/ex2/distance.c
+++ src/main/ex2/distance.c
@@ -1,5 +1,30 @@
 #include "headers/distance.h"
 
 int edit_distance(const char *s1, const char* s2) {
-    return 0;
+    if (strlen(s1) == 0)
+        return strlen(s2);
+
+    if (strlen(s2) == 0)
+        return strlen(s1);
+    
+    int d_noop = 0;
+    
+    if (s1[0] == s2[0])
+        d_noop = edit_distance(s1 + 1, s2 + 1);
+    else
+        d_noop = __INT_MAX__;
+    
+    int d_canc = 1 + edit_distance(s1, s2 + 1);
+
+    int d_ins = 1 + edit_distance(s1 + 1, s2);
+
+    int min = d_noop;
+
+    if (min < d_canc)
+        min = d_canc;
+
+    if (min < d_ins)
+        min = d_ins;
+
+    return min;
 }
