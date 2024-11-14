130c2118b1c5c8b1eea271decc946c94225703fe
Matteo Saracino <matteo.saracino@edu.unito.it>
2024-11-14 17:11:45 +0100

    Added ex2 directories

---
+++ src/main/ex2/distance.c
@@ -0,0 +1,5 @@
+#include "headers/distance.h"
+
+int edit_distance(const char *s1, const char* s2) {
+    return 0;
+}
---
+++ src/main/ex2/headers/distance.h
@@ -0,0 +1,9 @@
+#include <string.h>
+
+/**
+ * @brief Determine the minimum number of operations needed to trasform the string s2 into s1 (insertion and deletion)
+ * @param s1 string you want to get
+ * @param s2 string to transform
+ * @return sum of insertions and deletions
+ */
+int edit_distance(const char *s1, const char* s2);
